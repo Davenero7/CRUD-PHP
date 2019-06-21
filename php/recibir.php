8749a1014f41e744951df0b57b23f042d57a9734
davenero7 <dave7.spark@gmail.com>
2019-06-21 13:31:06 -0500

    Conexion con mysqli_connect

--- php/recibir.php
+++ php/recibir.php
@@ -1,35 +1,47 @@
 <?php
 
-//Los marcados con amarillos es el name del web (index)
+//Los marcados con verde es el name del web (index)
 	$nombre = $_POST['nombrew'];
-	$apellidop = $_POST['apellidopw'];
-	$apellidom = $_POST['apellidomw'];
+	$apellidopaterno = $_POST['apellidopw'];
+	/*
+$apellidom = $_POST['apellidomw'];
 	$edad = $_POST['edadw'];
 	$anio = $_POST['aniow'];
-	$fecha = $_POST['fechaw'];
+	$fecha = $_POST['fechaw'];*/
 
 	try {
 
-		$PDO=new PDO('mysql:host=localhost;port=port=3306;dbname=crudphp;','root','');
+		$conn = mysqli_connect("localhost", "root", "", "prueba");
+		//$PDO=new PDO('mysql:host=localhost;port=port=3306;dbname=crudphp;','root','');
+		if($conn === false){
+		    die("ERROR DE CONEXION " . mysqli_connect_error());
+		}
 
-		$sql=$PDO->prepare("INSERT INTO formulario (nombre,apellidop,apellidom,edad,anio,fecha) VALUES (:nombre, :apellidop, :apellidom, :edad, :anio, :fecha)");
-
-		$sql->bindParam(':nombre',$nombre);
+		$sql = "INSERT INTO datos (nombre, apellidopaterno) VALUES ('$nombre', '$apellidopaterno')";
+		//$sql=$conn->prepare("INSERT INTO formulario (nombre,apellidop,apellidom,edad,anio,fecha) VALUES (:nombre, :apellidop, :apellidom, :edad, :anio, :fecha)");
+		if(mysqli_query($conn, $sql)){
+		    echo "Datos guardados correctamente";
+		} else{
+		    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+		}
+	/*	$sql->bindParam(':nombre',$nombre);
 		$sql->bindParam(':apellidop',$apellidop);
 		$sql->bindParam(':apellidom',$apellidom);
 		$sql->bindParam(':edad',$edad);
 		$sql->bindParam(':anio',$anio);
 		$sql->bindParam(':fecha',$fecha);
-		$sql->execute();
+		$sql->execute(); */
 
 		echo '<script>
 		alert("datos enviados con exito");
 		</script>';
 
-		
+
+
+
 	} catch (PDOException $e) {
 		echo 'Error' .$e->getMessage();
 	}
 
-	
+
 ?>
